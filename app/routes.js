35aa781d6f51cfb45c42bc26fb164c9617d717f1
StephenHill-NHSBSA <stephen.hill9@nhs.net>
2021-11-18 16:03:54 +0000

    Added data preview and error screens

--- app/routes.js
+++ app/routes.js
@@ -6,3 +6,37 @@ const router = express.Router();
 // Add your routes here - above the module.exports line
 
 module.exports = router;
+
+// Are you pregnant?
+
+router.post('/v1/step-4', function (req, res) {
+
+    var step4Continue = req.session.data['step4']
+  
+    if (step4Continue === "yes") {
+      res.redirect('/v1/payment-runs/november-in-progress')
+    }
+    else if (step4Continue === "no") {
+      res.redirect('/v1/payment-runs/november')
+    }
+    else {
+      res.redirect('/v1/payment-runs/step-4')
+    }
+  
+  })
+
+  router.post('/v1/step-4-error', function (req, res) {
+
+    var step4Continue = req.session.data['step4']
+  
+    if (step4Continue === "yes") {
+      res.redirect('/v1/payment-runs/november-in-progress')
+    }
+    else if (step4Continue === "no") {
+      res.redirect('/v1/payment-runs/november')
+    }
+    else {
+      res.redirect('/v1/payment-runs/step-4-error')
+    }
+  
+  })
